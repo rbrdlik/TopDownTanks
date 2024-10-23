da2e17da505df331907312905bf919fd821a28c4
roman.brdlik@student.spsmb.cz <roman.brdlik@student.spsmb.cz>
2024-10-23 10:31:25 +0200

    update

--- TopDownTanks/obj/Debug/net8.0-windows/TopDownTanks.AssemblyInfo.cs
+++ TopDownTanks/obj/Debug/net8.0-windows/TopDownTanks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopDownTanks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbece29d90211ace47cbefc9b6157ec51dafbe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopDownTanks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopDownTanks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
