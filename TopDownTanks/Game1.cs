99d7adae81c0988b2f02099baaefc0af82895c94
rbrdlik <roman.brdlik@student.spsmb.cz>
2024-10-30 19:05:06 +0100

    Pridani strelby pomoci mysi a osetreni hranic obrazovky

--- TopDownTanks/Game1.cs
+++ TopDownTanks/Game1.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System.IO;
+using System.Collections.Generic;
+// ReSharper disable All
 
 namespace TopDownTanks;
 
@@ -11,6 +12,8 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Tank _tank;
     private Texture2D _backgroundTexture;
+    private List<Missile> _missiles;
+    private Texture2D _missileTexture;
 
     public Game1()
     {
@@ -23,19 +26,23 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
+        _missiles = new List<Missile>();
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        
+
         Texture2D tankTexture = Content.Load<Texture2D>("tank");
+        _backgroundTexture = Content.Load<Texture2D>("background");
+        
+        _missileTexture = new Texture2D(GraphicsDevice, 1, 1);
+        _missileTexture.SetData(new[] { Color.White });
+
         Vector2 startPosition = new Vector2(40, 290);
         float tankSpeed = 200f;
-        
-        _tank = new Tank(tankTexture, startPosition, tankSpeed);
-        _backgroundTexture = Content.Load<Texture2D>("background");
+        _tank = new Tank(tankTexture, startPosition, tankSpeed, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight, _missiles, _missileTexture);
     }
 
     protected override void Update(GameTime gameTime)
@@ -43,8 +50,18 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-        
+
         _tank.Update(gameTime);
+        
+        for (int i = _missiles.Count - 1; i >= 0; i--)
+        {
+            _missiles[i].Update(gameTime);
+            if (!_missiles[i].IsVisible)
+            {
+                _missiles.RemoveAt(i);
+            }
+        }
+
         base.Update(gameTime);
     }
 
@@ -55,6 +72,12 @@ public class Game1 : Game
         _spriteBatch.Begin();
         _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
         _tank.Draw(_spriteBatch);
+        
+        foreach (var missile in _missiles)
+        {
+            missile.Draw(_spriteBatch);
+        }
+
         _spriteBatch.End();
         base.Draw(gameTime);
     }
--- TopDownTanks/Tank.cs
+++ TopDownTanks/Tank.cs
@@ -1,46 +1,79 @@
-namespace TopDownTanks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+// ReSharper disable All
+
+namespace TopDownTanks;
 
 public class Tank
 {
     private Texture2D _texture;
     private Vector2 _position;
     private float _speed;
+    private int _screenWidth;
+    private int _screenHeight;
+    private List<Missile> _missiles;
+    private Texture2D _missileTexture;
+    private bool _isShooting;
 
-    public Tank(Texture2D texture, Vector2 position, float speed)
+    public Tank(Texture2D texture, Vector2 position, float speed, int screenWidth, int screenHeight, List<Missile> missiles, Texture2D missileTexture)
     {
         _texture = texture;
         _position = position;
         _speed = speed;
+        _screenWidth = screenWidth;
+        _screenHeight = screenHeight;
+        _missiles = missiles;
+        _missileTexture = missileTexture;
+        _isShooting = false;
     }
 
     public void Update(GameTime gameTime)
     {
         float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
         var keyboardState = Keyboard.GetState();
-
-        if (keyboardState.IsKeyDown(Keys.W))
+        var mouseState = Mouse.GetState();
+        
+        if (keyboardState.IsKeyDown(Keys.W) && _position.Y > 0)
         {
             _position.Y -= _speed * dt;
         }
-        if (keyboardState.IsKeyDown(Keys.S))
+        if (keyboardState.IsKeyDown(Keys.S) && _position.Y < _screenHeight - _texture.Height)
         {
             _position.Y += _speed * dt;
         }
-        if (keyboardState.IsKeyDown(Keys.A))
+        if (keyboardState.IsKeyDown(Keys.A) && _position.X > 0)
         {
             _position.X -= _speed * dt;
         }
-        if (keyboardState.IsKeyDown(Keys.D))
+        if (keyboardState.IsKeyDown(Keys.D) && _position.X < _screenWidth - _texture.Width)
         {
             _position.X += _speed * dt;
         }
+        
+        if (mouseState.LeftButton == ButtonState.Pressed && !_isShooting)
+        {
+            Shoot(new Vector2(mouseState.X, mouseState.Y));
+            _isShooting = true;
+        }
+        else if (mouseState.LeftButton == ButtonState.Released)
+        {
+            _isShooting = false;
+        }
+    }
+
+    private void Shoot(Vector2 targetPosition)
+    {
+        Vector2 tankRightCenter = new Vector2(_position.X + _texture.Width, _position.Y + _texture.Height / 2);
+        Vector2 direction = targetPosition - tankRightCenter;
+        direction.Normalize();
+
+        _missiles.Add(new Missile(_missileTexture, tankRightCenter, direction));
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(_texture, _position, null, Color.White);
+        spriteBatch.Draw(_texture, _position, Color.White);
     }
 }
